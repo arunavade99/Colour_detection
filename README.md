5347a477b9a060eb56fc82cf8a5209d98eee87c5
arunavade99 <arunavade99@gmail.com>
2025-07-30 21:03:20 +0530

    Readme file updated and upload 1st step codes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Colour_detection
+
+1st step, Select the colour using find_colour.py. Then using the colour range arrange the final code.
---
+++ masking.py
@@ -0,0 +1,40 @@
+# importing cv2 and numpy library
+import cv2
+import numpy as np
+
+# Reading an image
+img = cv2.imread('1.jpg')
+
+# The kernel to be used for dilation purpose
+kernel = np.ones((5, 5), np.uint8)
+
+# converting the image to HSV format
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# defining the lower and upper values of HSV,
+# this will detect pink colour
+Lower_hsv = np.array([132,45,118])
+Upper_hsv = np.array([179,193,255])
+
+# creating the mask by eroding, morphing,
+# dilating process
+Mask = cv2.inRange(hsv, Lower_hsv, Upper_hsv)
+Mask = cv2.erode(Mask, kernel, iterations=1)
+Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, kernel)
+Mask = cv2.dilate(Mask, kernel, iterations=1)
+
+# Inverting the mask by
+# performing bitwise-not operation
+# Mask = cv2.bitwise_not(Mask)
+
+# Displaying the image
+cv2.imshow('Mask', Mask)
+cv2.imshow('Image', img)
+
+# waits for user to press any key
+# (this is necessary to avoid Python
+# kernel from crashing)
+cv2.waitKey(0)
+
+# closing all open windows
+cv2.destroyAllWindows()
